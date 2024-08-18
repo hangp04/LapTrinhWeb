62bf2bffbc87e0b31fbff96b4d2d0c46da33ac95
hangp04 <152256467+hangp04@users.noreply.github.com>
2024-08-18 21:17:30 +0700

    Create bai1.php

---
+++ Buoi_1/bai1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>Bài 1</title>
+    <link rel="stylesheet" href="../Buoi_1/bai1.css">
+</head>
+<body>
+    <?php
+        echo "<p><b>Hello</b>, World!</p>";
+    ?>
+    <div class="return-home">
+        <a href="../index/index.html" class="return"><i class="fa fa-arrow-left" aria-hidden="true"></i></a>
+    </div>
+</body>
+</html>
