ef3d6288d474b7408e891868d1c1d221f966b5d1
hangp04 <152256467+hangp04@users.noreply.github.com>
2024-08-27 23:10:48 +0700

    Update bai4.php

--- Buoi_2/bai4.php
+++ Buoi_2/bai4.php
@@ -1,1 +1,24 @@
+<?php 
+    $array = [];
+    $LonNhat = $NhoNhat = $Tong = 0;
+    $arraySapXep = [];
+    $SoCanTim = "";
+    $Found = false;
 
+    if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+        $arrayInput = isset($_GET['array']) ? $_GET['array'] : '';
+        $array = array_map('intval', explode(" ", $arrayInput));
+
+        if(!empty($array)) {
+            $LonNhat = max($array);
+            $NhoNhat = min($array);
+            $Tong = array_sum($array);
+
+            $arraySapXep = $array;
+            sort($arraySapXep);
+
+            $SoCanTim = isset($_GET['So_Can_Tim']) ? (int)$_GET['So_Can_Tim'] : '';
+            $Found = in_array($SoCanTim, $array);
+        }
+    }
+?>
